7756f224f053b0c1014b5cac9f05a330129f275f
redmac135 <ethanzhao1981@gmail.com>
2024-03-11 00:58:38 -0400

    update title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ethan's LinAlg Notes",
+    pageTitle: "Ethan's Fun Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
